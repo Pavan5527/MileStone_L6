e9aa12b1735bd588a79867a002a64b06fa2c9dc3
Pavan5527 <85097031+Pavan5527@users.noreply.github.com>
2022-12-24 23:06:04 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,6 +1,6 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => 
   class Todo extends Model {
     /**
      * Helper method for defining associations.
