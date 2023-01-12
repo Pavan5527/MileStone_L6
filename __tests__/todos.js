51833a8de49fa714666e561bdc6a078747b6aa44
Vadagam Pavan Kumar <85097031+Pavan5527@users.noreply.github.com>
2023-01-13 01:16:23 +0530

    Update todos.js

--- __tests__/todos.js
+++ __tests__/todos.js
@@ -53,9 +53,25 @@ describe("Todo Application", function () {
     expect(parsedUpdateResponse.completed).toBe(true);
   });
 
+   test("Fetches all todos in the database using /todos endpoint", async () => {
+    await agent.post("/todos").send({
+      title: "Buy xbox",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    await agent.post("/todos").send({
+      title: "Buy ps3",
+      dueDate: new Date().toISOString(),
+      completed: false,
+    });
+    const response = await agent.get("/todos");
+    const parsedResponse = JSON.parse(response.text);
 
+    expect(parsedResponse.length).toBe(4);
+    expect(parsedResponse[3]["title"]).toBe("Buy ps3");
+  });
 
-  test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
+    test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
     // FILL IN YOUR CODE HERE
     const response = await agent.post("/todos").send({
       title: "Buy Everything",
